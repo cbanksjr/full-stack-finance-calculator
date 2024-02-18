46db0d987ecbc46f1a08bd0673006cc437c5585e
cbanksjr <craigbjr241@gmail.com>
2024-02-17 23:28:58 -0500

    account component renders on refresh

--- frontend/src/components/AccountComponent.jsx
+++ frontend/src/components/AccountComponent.jsx
@@ -1,27 +1,38 @@
-import {useState} from "react";
-import axios from 'axios';
+/** @format */
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const AccountComponent = () => {
-    const [amount, setAmount] = useState([])
+  const [account, setAccount] = useState([]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        await axios.get("http://localhost:8080/api/account/setAmount/{id}")
-            .then((response) => setAmount(response.data))
-            .catch((error) => console.error(error))
+  useEffect(() => {
+    const fetchAccountData = async () => {
+      await axios
+        .get("http://localhost:8080/api/account/setAmount/1")
+        .then((response) => {
+          console.log(response.data);
+          setAccount(response.data);
+        })
+        .catch((error) => console.error(error));
+    };
+    return () => fetchAccountData();
+  }, []);
 
-        return (
-            <section className="bg-amber-500 col-start-1 text-white">
-                {amount.map((element) => (
-                        <div key={element.id} className="text-white">
-                            <p>Account: {element.amount}</p>
-                            <p>Deducted: {element.deducted}</p>
-                            <p>Remaining: {element.remaining}</p>
-                            <button onClick={handleSubmit}>Add To Account</button>
-                        </div>
-                ))}
-            </section>
-        )
-    }
-}
+  return (
+    <section className="flex flex-col justify-center col-start-1 row-start-2 text-black rounded-lg bg-white">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-xl font-semibold mb-4">Account</h1>
+        {account.map((element, index) => (
+          <div key={element.id || index} className="text-black">
+            <p>Amount: {element.amount}</p>
+            <p>Deducted: {element.deducted}</p>
+            <p>Remaining: {element.remaining}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default AccountComponent
+export default AccountComponent;
