171fb4703325ea1bf866c1ef2df7f341dd6564f1
cbanksjr <craigbjr241@gmail.com>
2024-03-09 16:04:28 -0500

    created subtraction input

--- client/src/components/SubtractAllocationInput.jsx
+++ client/src/components/SubtractAllocationInput.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import SubmitButton from './SubmitButton'
+import React, {useState, useEffect} from 'react'
+import SavingsButton from './SavingsButton'
+import axios from 'axios'
 
 const SubtractAllocationInput = ({title}) => {
+  const [allocationTakenOut, setAllocationTakenOut] = useState([]);
+
+    const updateSavingsData = async (e) => {
+      e.preventDefault();
+      const savingsData = {allocationTakenOut};
+      try {
+            const response = await axios.put("http://localhost:8080/api/savings/updateSavings/1", savingsData); 
+            const data = response.data;
+            setAllocationTakenOut(data);
+            console.log(data);
+        } catch (err) {
+            console.error("Error fetching savings data: ", err.message);
+        }
+    }
+
+    useEffect(() => {
+        updateSavingsData();
+    }, []);
   return (
     <form className='flex flex-col items-center mt-10'>
         <h1 className='text-xl font-semibold pt-10'>{title}</h1>
         <input
             type="number"
-            placeholder="Take out of allocation"
+            placeholder="Amount"
+            value={allocationTakenOut}
+            onChange={(e) => setAllocationTakenOut(e.target.value)}
             className="border-4 rounded-md p-2 text-center mt-4" />
-            <SubmitButton name="Submit" />
+
+            <SavingsButton name="Update Savings" updateSavings={updateSavingsData} />
     </form>
   )
 }
