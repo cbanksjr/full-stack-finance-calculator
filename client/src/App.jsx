40d5dfdaa4970af9c6f1e4ac8086afddad4364e2
cbanksjr <craigbjr241@gmail.com>
2024-03-22 00:15:49 -0500

    added BigDecimal to fix decimal issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import SubtractAllocationInput from "./components/SubtractAllocationInput.jsx";
 
 const App = ({ showAccount }) => {
   return (
-    <main className="p-16">
-      <div className="flex flex-col items-center bg-gradient-to-tl from-blue-300 to-blue-600 rounded-lg p-12 shadow-lg shadow-gray-500">
+    <main>
+      <div className="flex flex-col items-center bg-gradient-to-tl from-blue-300 to-blue-600 shadow-gray-500">
         <Form />
       </div>
     </main>
