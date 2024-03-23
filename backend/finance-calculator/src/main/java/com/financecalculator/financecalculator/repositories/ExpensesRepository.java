4ff976b5221bce2ff254d170c7d70ea3039590d4
cbanksjr <craigbjr241@gmail.com>
2024-03-23 11:47:34 -0500

    fixed Respository spelling, updated savings test, created expenses test

--- backend/finance-calculator/src/main/java/com/financecalculator/financecalculator/repositories/ExpensesRepository.java
+++ backend/finance-calculator/src/main/java/com/financecalculator/financecalculator/repositories/ExpensesRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.financecalculator.financecalculator.models.Expenses;
 
-public interface ExpensesRespository extends CrudRepository<Expenses, Long>{
+public interface ExpensesRepository extends CrudRepository<Expenses, Long>{
     
 }
