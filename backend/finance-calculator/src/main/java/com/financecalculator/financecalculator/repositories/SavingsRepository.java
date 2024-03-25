2448810cf21b461aa839685038c250ca8efa7e0a
cbanksjr <craigbjr241@gmail.com>
2024-03-25 12:19:28 -0500

    fixed responsive design

--- backend/finance-calculator/src/main/java/com/financecalculator/financecalculator/repositories/SavingsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.financecalculator.financecalculator.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.financecalculator.financecalculator.models.Savings;
-
-public interface SavingsRepository extends CrudRepository<Savings, Long>{
-    
-}
